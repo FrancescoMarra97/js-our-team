8b15426aee083add9725a803936fef7418013841
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-14 16:03:17 +0200

    inserimento in pagina + fix variabile img

--- assets/script.js
+++ assets/script.js
@@ -3,41 +3,43 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "assets/img/female3.png"
   }
 ];
 
 const teamEl = document.getElementById("team");
+
+let finalMarkup= " ";
 
 for (let i = 0; i < teamMembers.length; i++) {
   const member = teamMembers[i];
@@ -45,11 +47,12 @@ for (let i = 0; i < teamMembers.length; i++) {
   
   const markup = generateTeamMember(member);
   console.log(markup);
-  
-  
+
+  finalMarkup += markup
+ 
 }
 
-
+ teamEl.innerHTML=finalMarkup;
 
 
 
@@ -60,14 +63,14 @@ for (let i = 0; i < teamMembers.length; i++) {
 
 
 function generateTeamMember(member) {
-  const {image, name, role, email} = member;
+  const {img, name, role, email} = member;
 
   return `
     <div  class="col-md-4">
                     <div class="card text-bg-dark mb-3" style="max-width: 540px;">
                         <div class="row g-0">
                           <div class="col-md-4">
-                            <img src="${image}" class="img-fluid rounded-start" alt="...">
+                            <img src="${img}" class="img-fluid rounded-start" alt="...">
                           </div>
                           <div class="col-md-8">
                             <div class="card-body">
